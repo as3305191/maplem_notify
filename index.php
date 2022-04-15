9e75c27ae80cab2b37bdfd291ea64d514b1d79f2
as3305191 <67482941+as3305191@users.noreply.github.com>
2022-04-15 12:43:47 +0800

    Add files via upload

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ function index(){
         'Authorization:Bearer JVtjbu74jAWWb6QxvLRbZSD3019wrUP4qQNSdPAEKkC'
     );
 
-    $key = json_encode($_GET["key"], true);
+    $key = json_decode($_GET["key"], true);
     $msg = json_decode(file_get_contents('php://input'), true);
   
     $message = array(
