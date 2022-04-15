e561febf75d7b7f573cb6b83ed6841884de612ba
as3305191 <67482941+as3305191@users.noreply.github.com>
2022-04-15 10:44:53 +0800

    Add files via upload

--- index.php
+++ index.php
@@ -1,16 +1,24 @@
 <?php
-$headers = array(
-    'Content-Type: multipart/form-data',
-    'Authorization:Bearer JVtjbu74jAWWb6QxvLRbZSD3019wrUP4qQNSdPAEKkC'
-);
-$message = array(
-    'message' => 'Hello,123~'
-);
-$ch = curl_init();
-curl_setopt($ch , CURLOPT_URL , "https://notify-api.line.me/api/notify");
-curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-curl_setopt($ch, CURLOPT_POST, true);
-curl_setopt($ch, CURLOPT_POSTFIELDS, $message);
-$result = curl_exec($ch);
-curl_close($ch);
+
+
+function index(){
+    $message = array(
+        'message' => $_POST["msg"]
+    );
+
+    $headers = array(
+        'Content-Type: multipart/form-data',
+        'Authorization:Bearer '.$_POST["key"]
+    );
+    
+    $ch = curl_init();
+    curl_setopt($ch , CURLOPT_URL , "https://notify-api.line.me/api/notify");
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $message);
+    $result = curl_exec($ch);
+    curl_close($ch);
+
+}
+
 ?>
